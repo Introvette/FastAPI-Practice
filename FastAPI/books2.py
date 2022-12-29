e61a268b99a021d26b4229ec3b31ada3f3d63a6b
yvette <vette.rosario@gmail.com>
2022-12-29 10:55:56 -0500

    Made description property optional using "Optional" import from typing

--- FastAPI/books2.py
+++ FastAPI/books2.py
@@ -1,14 +1,17 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from uuid import UUID
+from typing import Optional
 
 app = FastAPI()
 
 class Book(BaseModel):
     id: UUID
-    title: str
+    title: str = Field(min_length=1)
+    # adding Field to a property value makes it so data is validated correctly
+    # and can't be an empty string when running it on swagger, kind of like Django models
     author: str
-    description: str
+    description: Optional[str] = Field(title="Description of the book", max_length=100, min_length=1, )
     rating: int
 
 
@@ -17,3 +20,9 @@ BOOKS = []
 @app.get("/")
 async def read_all_books():
     return BOOKS
+
+@app.post("/")
+async def create_book(book: Book):
+    # book of type Book (class we made)
+    BOOKS.append(book)
+    return book
