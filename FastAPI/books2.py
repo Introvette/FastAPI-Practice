5ba472264073c3bd0e54824b175b47a505021224
yvette <vette.rosario@gmail.com>
2022-12-29 11:01:37 -0500

    Added field for rating to provide a limit

--- FastAPI/books2.py
+++ FastAPI/books2.py
@@ -5,21 +5,28 @@ from typing import Optional
 
 app = FastAPI()
 
+
 class Book(BaseModel):
     id: UUID
     title: str = Field(min_length=1)
     # adding Field to a property value makes it so data is validated correctly
     # and can't be an empty string when running it on swagger, kind of like Django models
-    author: str
-    description: Optional[str] = Field(title="Description of the book", max_length=100, min_length=1, )
-    rating: int
+    author: str = Field(min_length=1, max_length=100)
+    description: Optional[str] = Field(title="Description of the book", max_length=100, min_length=1)
+    rating: int = Field(gt=-1, lt=101)
+    # this field for rating means the rating needs to be greater than (gt) -1
+    # and less than (lt) 101
+    # rating should never be over 100
+
 
 
 BOOKS = []
 
+
 @app.get("/")
 async def read_all_books():
     return BOOKS
+
 
 @app.post("/")
 async def create_book(book: Book):
