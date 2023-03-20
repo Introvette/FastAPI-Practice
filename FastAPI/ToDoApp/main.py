a6bd4dcecc560164a66c386adbc00aeb675b0ad6
Yvette Rosario <vette.rosario@gmail.com>
2023-03-20 15:02:44 -0400

    create todo

--- FastAPI/ToDoApp/main.py
+++ FastAPI/ToDoApp/main.py
@@ -1,5 +1,5 @@
-# from http.client import HTTPException
-
+from typing import Optional
+from pydantic import BaseModel, Field
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 import models
@@ -17,6 +17,12 @@ def get_db():
         db.close()
 # ^^ this will execute after read_all
 
+class Todo(BaseModel):
+    title: str
+    description: Optional[str]
+    priority: int = Field(gt=0, lt=6, description="The priority must be between 1-5")
+    complete: bool
+
 @app.get("/")
 async def read_all(db: Session = Depends(get_db)):
     return db.query(models.Todos).all()
@@ -27,6 +33,23 @@ async def read_todo(todo_id: int, db: Session = Depends(get_db)):
     if todo_model is not None:
         return todo_model
     raise http_exception() # http_exception function
+
+@app.post("/")
+async def create_todo(todo: Todo, db: Session = Depends(get_db)):
+    todo_model = models.Todos()
+    todo_model.title = todo.title
+    todo_model.description = todo.description
+    todo_model.priority = todo.priority
+    todo_model.complete = todo.complete
+
+    db.add(todo_model)
+    db.commit()
+
+    return {
+        'status': 201,
+        # 201 means the request has been fulfilled
+        'transaction': 'Successful'
+    }
 
 def http_exception():
     return HTTPException(status_code=404, detail="Todo not found")
