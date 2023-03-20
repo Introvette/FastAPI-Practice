2acc1a35d6389c0493caf11c72059fe44baa67dc
Yvette Rosario <vette.rosario@gmail.com>
2023-03-20 14:49:39 -0400

    get by id

--- FastAPI/ToDoApp/main.py
+++ FastAPI/ToDoApp/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, Depends
+# from http.client import HTTPException
+
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 import models
 from database import engine, SessionLocal
@@ -18,4 +20,14 @@ def get_db():
 @app.get("/")
 async def read_all(db: Session = Depends(get_db)):
     return db.query(models.Todos).all()
+
+@app.get("/todo/{todo_id}")
+async def read_todo(todo_id: int, db: Session = Depends(get_db)):
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise http_exception() # http_exception function
+
+def http_exception():
+    return HTTPException(status_code=404, detail="Todo not found")
 
